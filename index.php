8b737a296a0678caa8ce2dfe8e81757ba5973d02
123-LP <85690063+123-LP@users.noreply.github.com>
2021-06-10 17:04:47 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="de">
+	<head>
+		<meta charset="utf-8"/>
+	<title> Quiz </title>
+	
+	
+	</head>
+	<body>
+		<h1> Quiz Anmeldung </h1>
+		<form action="a.php" method"post">
+		<table>
+			<tr>
+				<th>Benutzername</th>
+				<th><input benutzername="Benutzername"></th>
+			</tr>
+				<th>Passwort</th> 
+				<th><input passwort="Passwort"></th>
+			</tr>
+			<tr>
+				<th><input type="submit" value="klick mich" onclick="<?php ?>" ></th>	
+			</tr>
+			<tr>
+				<th>Registrieren</th>
+				<th>www.luo-darmstadt.de</th>
+			</tr>
+		</table>
+		</form>
+	</body>
+</html>
+<!--<?php $benutzername = $_POST["Benutzername:"]; ?><?php $passwort = $_POST["Passwort

eb381f6d31d3770d12521c25c23c116c9f5033cd
JonasCentarti <85689901+JonasCentarti@users.noreply.github.com>
2021-06-10 16:13:47 +0200

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="de">
+	<head>
+		<meta charset="utf-8"/>
+	<title> Quiz </title>
+	
+	
+	</head>
+	<body>
+		<h1> Quiz Anmeldung </h1>
+		<?php
+			$benutzername = $_POST["Benutzername:"];
+			$passwort = $_POST["Passwort:"];
+		?>
+		
+	</body>
+</html>

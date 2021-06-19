c522e3c11b4bd914f17e19be9b06b88571b73c94
123-LP <85690063+123-LP@users.noreply.github.com>
2021-06-19 18:00:15 +0200

    Add files via upload

---
+++ eigene_fragen.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+if(!isset($_SESSION['aktueller_benutzer'])) {
+    die('Bitte zuerst <a href="index.php">einloggen</a>');
+}
+$user = $_SESSION['aktueller_benutzer'];
+?>
+
+<!doctype html>
+	<html lang="de">
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="quizsheet.css">
+		<title>Eigene Fragen</title>
+	</head>
+	
+	<body>
+		<table>
+			<tr>
+				<td><h1>Quizlein</h1> </td>
+				<td><button id=profil><a href="profil.php"><?php echo $user ?></a></button></td>
+			</tr>
+		</table>
+		<div id=startLinks>
+			<button><a href="startseite.php">Startseite</a></button>
+			<button><a href="fragenset.php">Fragenset</a></button>
+			<button><a href="eigene_fragen.php">Eigene Fragen</a></button>
+			<button><a href="spielerliste.php">Spielerliste</a></button>
+			<button><a href="hilfe.php">Hilfe</a></button>
+			<button>Abmelden</button>
+		</div>
+	</body>
+	
+</html>

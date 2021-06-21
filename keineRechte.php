e880f0aae854e518113082bdb6afb97d1d4ad7e1
JonasCentarti <85689901+JonasCentarti@users.noreply.github.com>
2021-06-21 10:25:03 +0200

    Add files via upload

---
+++ keineRechte.php
@@ -0,0 +1,6 @@
+<!doctype html>
+<html lang="de">
+<td><h1>ERROR</h1> </td> 
+<th>Du hast keine Rechte um auf diese Seite zuzugreifen</th>
+<br> </br>
+<button><a href="startseite.php">Zurück zur Startseite</a></button>

451f86745d0ac31bc9e5336c9fa787f6c4069f39
JonasCentarti <85689901+JonasCentarti@users.noreply.github.com>
2021-06-25 13:56:37 +0200

    Add files via upload

---
+++ befoerdern.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="de">
+<head>
+	<meta charset="utf-8"/>
+	<title>Benutzer befördern</title>
+
+</head>
+<body>
+  <h1>Benutzer befördern</h1>
+    <p> Benutzer wurde erfolgreich befördert </p>
+  
+	
+	<hr/>
+	<link rel="stylesheet" href="quizsheet.css">
+	<p> <a href="befoerdern1.php"> Zurück zur Eingabe</a></p>
+	<p> <a href="startseite.php"> Zurück zur Startseite</a></p>
+	</body>
+	</html>
---
+++ befoerdern1.php
@@ -0,0 +1,76 @@
+
+<?php
+
+session_start();
+if(!isset($_SESSION['aktueller_benutzer'])) {
+    die('Bitte zuerst <a href="index.php">einloggen</a>');
+}
+$user = $_SESSION['aktueller_benutzer'];
+$rang = $_SESSION['rank'];
+if ($rang!=="god"){
+	header('Location: keineRechte.php');
+}	
+	if(isset($_GET['eingegeben'])) { 
+	 
+	$benutzername=$_POST["benutzername"];
+	$passwort=$_POST["passwort"];
+	$benutzernameBefoerderter=$_POST["benutzernameBefoerderter"];
+	$telefonnummer=$_POST["telefonnummer"];
+
+	if($benutzername==null || $passwort==null || $benutzernameBefoerderter==null || $telefonnummer==null){
+		echo '<dialog open>Bitte gib vollständige Daten an</br> <a href="befoerdern1.php">ja</a> </dialog>';
+	}
+	else{
+	$connection = new mysqli("localhost", "root", "", "quiz");
+	
+		$sql="INSERT INTO `moderator` (`Benutzername`, `Telefonnummer`) VALUES ('$benutzernameBefoerderter', '$telefonnummer');";
+		$result=$connection->query($sql);
+		//Sessiondaten speichern und weiterleiten
+		header('Location: befoerdern.php');
+	}
+	
+
+	$sql="SELECT Benutzername FROM benutzer where Benutzername= '$benutzername' and Passwort='$passwort' and benutzernameBefoerderter='$benutzernameBefoerderter'"; //prüfen ob benutzer
+	
+	
+	
+}
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+<meta charset="utf-8"/>
+<link rel="stylesheet" href="quizsheet.css">
+<title>Benutzer befördern</title>
+</head>
+<body>
+<h1> Benutzer befördern </h1>
+
+<h2> Bitte trage die Daten ein: </h2>
+<form action="?eingegeben=1" method="post">
+	<table>
+	  <tr><td>Benutzername: </td>
+			<td> <input name="benutzername"></td></tr>
+			
+	  <tr><td>Passwort: </td>
+			<td> <input name="passwort"></td></tr>
+			
+	  <tr><td>Zu befördernder Nutzer: </td>
+			<td> <input name="benutzernameBefoerderter"></td></tr>
+			
+	  <tr><td>Telefonnummer des Nutzers: </td>
+			<td> <input name="telefonnummer"></td></tr>
+			
+		<tr><td></td>
+		  <tr><td><input type="submit"></td></tr>
+		     
+	</table>
+	</form>
+
+</body>
+</html>
